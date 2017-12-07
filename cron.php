c1ffea5e004d9285e1243e2d5751fabfebb926be
flydev-fr <flydev@sekretservices.com>
2017-12-07 15:20:05 +0100

    Removed HookEvent argument from ___cronJob()

--- cron.php
+++ cron.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../../../index.php');
 if($wire->modules->isInstalled('Duplicator')) {
     $cron = $wire->modules->get('Duplicator');
     if($cron) {
-        $e = new HookEvent();
-        $cron->cronJob($e);
+        //$e = class_exists('\ProcessWire\HookEvent') ? new \ProcessWire\HookEvent() : new HookEvent();
+        $cron->cronJob(/*$e*/);
     }
 }

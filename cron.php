206045f115a0c14e26fa2190ef0388baa92831d0
flydev-fr <flydev@sekretservices.com>
2017-12-09 13:42:20 +0100

    removed HookEvent

--- cron.php
+++ cron.php
@@ -4,7 +4,6 @@ require_once(__DIR__ . '/../../../index.php');
 if($wire->modules->isInstalled('Duplicator')) {
     $cron = $wire->modules->get('Duplicator');
     if($cron) {
-        //$e = class_exists('\ProcessWire\HookEvent') ? new \ProcessWire\HookEvent() : new HookEvent();
-        $cron->cronJob(/*$e*/);
+        $cron->cronJob();
     }
 }
